78be22d252a007447fc66301219d511dbfa70e14
Filippo Fedeli <41326957+SirPopiel@users.noreply.github.com>
2018-12-09 16:21:01 +0100

    Final (quasi)

--- projet2.py
+++ projet2.py
@@ -5,12 +5,25 @@ Created on Fri Nov 23 17:19:23 2018
 
 @author: 3803008
 """
+
 import re
 
 from gurobipy import *
 import numpy as np
+import timeit
+import optdistr
+import matplotlib.pyplot as plt
+import pandas as pd
 
-cities = (1,13,28)
+start = timeit.default_timer()
+
+#cities = (12, 15, 28, 29, 32)
+#cities=(12, 13, 15, 16, 28)
+cities=(1, 13, 28)
+#cities=(13, 15, 26, 34)
+#cities=(5, 15, 17, 28, 32)
+#cities=(15, 26, 28, 29, 32)
+cities=sorted(cities)
 k=len(cities)
 
 #recuperations des populations des villes 
@@ -80,7 +93,6 @@ for y in range(0,n*k,k):
 
     matrice_contraintes.append(l2)
 
-print(matrice_contraintes)
 
 #Les contraintes concernant la population des villes 
 co_pop = []
@@ -103,7 +115,7 @@ for j in range(n):
 	for i in cities:
 		ci.append(dis[j][i])
 
-print(ci)
+#print(len(c))
 
 m=[]
 l2 =[]
@@ -126,7 +138,7 @@ for i in range (0,len(ci),k):
 matrice_contraintes.extend(co_pop)
 matrice_contraintes.extend(m)
 
-#print(matrice_contraintes)
+#print(len(matrice_contraintes))
 #print(len(matrice_contraintes[0]))
 
 ###########################SECONDE MEMBRE###############################
@@ -146,17 +158,16 @@ for i in range(k):
 
 for i in range(n):
     b.append(0)
-print(len(b))
+#print(len(b))
 
 ######################FONCTION OBJECTIVE################################
 c = []
 epsilon=1e-6
- 
 for j in range(n):
     for i in cities:
         c.append(epsilon*dis[j][i])
 c.append(1)
-
+cici=c
 #print(c)
 
 ###############################GUROBI####################################
@@ -186,7 +197,7 @@ m1.setObjective(obj,GRB.MINIMIZE)
 # Definition des contraintes
 for i in range(0,n):
     m1.addConstr(quicksum(matrice_contraintes[i][j]*x[j] for j in colonnes) == b[i], "Contrainte%d" % i)
-    print(i,j)
+    #print(i,j)
 
 for i in range(n,n+k):
     m1.addConstr(quicksum(matrice_contraintes[i][j]*x[j] for j in colonnes) <= b[i], "Contrainte%d" % i)
@@ -195,8 +206,10 @@ for i in range(n+k,n*2+k):
     m1.addConstr(quicksum(matrice_contraintes[i][j]*x[j] for j in colonnes) <= b[i], "Contrainte%d" % i)
     
 # Resolution
+#m1.Params.BranchDir= 1
 m1.optimize()
 
+#ImproveStartGap=0.3
 
 print("")                
 print('Solution optimale:')
@@ -215,27 +228,52 @@ print('g', '=', x[k].x)
 print("")
 print('Valeur de la fonction objectif :', m1.objVal) 
 
-###########################MATTHEWS' PARSING CODE###############################
-def parseCoord():
-    f = open("coordvilles92.txt", "r")
-    my_text = f.readlines()
-    return map(lambda coords: map(lambda coord : int(coord), re.findall(r'\d+', coords)), my_text)
+data = pd.read_csv('coordvilles92.txt', header=None)
+data.columns = ["Villes", "x", "y"]
+
+HdS=plt.imread("92.png")
+fig, ax = plt.subplots()
+ax.imshow(HdS)
+
+def connectpoints(x,y,p1,p2):
+    x1, x2 = x[p1], x[p2]
+    y1, y2 = y[p1], y[p2]
+    plt.plot([x1,x2],[y1,y2],'k-')
 
 
-###########################PE PART###############################
-"""
+k=0
+for i in range(n):
+    for j in cities:
+        if x[k].x==1:
+            plt.plot(data["x"][j], data["y"][j], 'ro-')    
+            connectpoints(data["x"],data["y"],i,j)
+        k=k+1
+
 Sat=0
 Satv=[]
+Sattest=[]
+#Sat=1e6*(m1.objVal-x[nbvar-1].x) #instead of cycling we can use this
 for j in range(nbvar-1):
     Sat+= 1/epsilon*c[j] * x[j].x
-    Satv.append(1/epsilon*c[j] * x[j].x)
+    Sattest.append(Sat)
+    if x[j].x==1:
+        Satv.append(1/epsilon*c[j] * x[j].x)
 SatM=Sat/n
 MinSat=max(Satv)
+ind=[i for i, j in enumerate(Satv) if j == MinSat]
+print(ind)
 
 
-exec(open("projet1(1).py").read())
+activehubs=cities
+(Sat1,Satv1)=optdistr1(activehubs)
+print(Sat)
 PE=1-Sat1/Sat
 print("PE",'=',PE)
 
+
+
+stop = timeit.default_timer()
+
+print('Time: ', stop - start) 
+
 #ma =m1.write("qa.lp")
-"""
